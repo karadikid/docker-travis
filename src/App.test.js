b404df0cc8b6f795c53d9150f88c16533114634d
Karadi Kid <goukaradi@hotmail.com>
2020-08-25 14:24:14 -0400

    travis ci commit.

---
+++ src/App.test.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import App from './App';
+
+test('renders learn react link', () => {
+  const { getByText } = render(<App />);
+  const linkElement = getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+// Duplicate Tests to show multiple tests
+
+test('renders learn react link', () => {
+  const { getByText } = render(<App />);
+  const linkElement = getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
